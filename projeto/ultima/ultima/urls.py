9116be798bf35a3bdd3ad0a56cf9e58a88d475f6
Myrian_Fernanda <myrian.fernanda@academico.ufpb.br>
2023-07-05 16:22:50 -0300

    organização

--- projeto/ultima/ultima/urls.py
+++ projeto/ultima/ultima/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from base.views import inicio 
 
-# from base.views import inicio
 urlpatterns = [
     # path('', inicio, name = 'inicio')
     path('admin/', admin.site.urls),
